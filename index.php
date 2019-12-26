b2334433bbe336942a43924efd8e6877782dcfe0
Dedy ibrahim <dedyibrahym23@gmail.com>
2019-12-26 19:18:37 +0700

    26/12/2019

--- index.php
+++ index.php
@@ -36,33 +36,7 @@
  * @filesource
  */
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
 switch (ENVIRONMENT)
 {
 	case 'development':
